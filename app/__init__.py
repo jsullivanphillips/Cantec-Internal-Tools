687ca1df9b8ee552202df48ccd0bbca844ff8a88
Jamie Sullivan-Phillips <j.sullivan-phillips@cantec.ca>
2025-04-03 15:25:51 -0700

    working on fixing heroku crash

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,10 @@ def create_app():
                 static_folder=static_path)
     app.config.from_object(Config)
     app.secret_key = Config.SECRET_KEY
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    database_url = os.environ.get("DATABASE_URL")
+    if database_url and database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
